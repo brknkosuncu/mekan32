7e27fb9246820598b28c8f73adc5249f64a22735
Berkan <bkosuncu@gmail.com>
2020-12-28 01:28:47 +0300

    sss

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 require('./app_api/models/db'); 
-var favicon = require('serve-favicon')
 var express = require('express');
+var favicon = require('serve-favicon')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -13,8 +13,9 @@ var usersRouter = require('./app_server/routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_server', 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
+app.use(favicon(path.join(__dirname, 'public', 'images','favicon.ico')))
 
 app.use(logger('dev'));
 app.use(express.json());
